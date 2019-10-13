796a19f49111c0b0c3824911f23e2fab4ce5de17
leelavathikm1 <leelavathikm1@gmail.com>
2019-10-13 15:16:03 +0530

    Adding For Loops

---
+++ Javascript/data.js
@@ -0,0 +1,92 @@
+var data = [
+  {
+    fname: "Erik",
+    lname: "Lee",
+    tel: "(838)260-6022",
+    address: "8615 Placerat St",
+    city: "Brandon",
+    state: "SC",
+    zip: 73748
+  },
+  {
+    fname: "Myra",
+    lname: "Woolverton",
+    tel: "(793)515-0225",
+    address: "1039 Molestie Ave",
+    city: "Golden",
+    state: "NC",
+    zip: 82942
+  },
+  {
+    fname: "Ravi",
+    lname: "Eckhart",
+    tel: "(656)388-1463",
+    address: "5671 Consequat Ln",
+    city: "Joliet",
+    state: "LA",
+    zip: 27555
+  },
+  {
+    fname: "Betty",
+    lname: "Buikema",
+    tel: "(318)250-4635",
+    address: "3256 Amet Dr",
+    city: "Tucson",
+    state: "RI",
+    zip: 15170
+  },
+  {
+    fname: "Dewey",
+    lname: "Knowles",
+    tel: "(213)847-8745",
+    address: "4630 Ac Dr",
+    city: "Spokane",
+    state: "AR",
+    zip: 19397
+  },
+  {
+    fname: "Terrence",
+    lname: "Phuan",
+    tel: "(892)612-4993",
+    address: "6220 Libero Ln",
+    city: "Trinity",
+    state: "KS",
+    zip: 61787
+  },
+  {
+    fname: "Penny",
+    lname: "Rende",
+    tel: "(752)880-5546",
+    address: "901 Porttitor Ln",
+    city: "Patterson",
+    state: "MO",
+    zip: 60968
+  },
+  {
+    fname: "Sherrie",
+    lname: "Grimsley",
+    tel: "(918)930-5946",
+    address: "9894 Sollicitudin St",
+    city: "Ashburn",
+    state: "WA",
+    zip: 93328
+  },
+  {
+    fname: "Arun",
+    lname: "Young",
+    tel: "(114)476-8416",
+    address: "2005 Magna Dr",
+    city: "Flushing",
+    state: "CO",
+    zip: 84399
+  },
+  {
+    fname: "Iris",
+    lname: "Pillai",
+    tel: "(212)353-3013",
+    address: "8660 Placerat Ave",
+    city: "Parker",
+    state: "PA",
+    zip: 36565
+  }
+];
