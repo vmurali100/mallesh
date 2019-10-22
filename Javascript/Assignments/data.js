c5e606a6ff2ecd9403a07108c77ceccf687ba76f
leelavathikm1 <leelavathikm1@gmail.com>
2019-10-22 16:46:15 +0530

    Adding Assignment3

---
+++ Javascript/Assignments/data.js
@@ -0,0 +1,55 @@
+var data = [
+  {
+    no: 1,
+    fname: "Terrence",
+    lname: "Phuan",
+    tel: "(892)612-4993",
+    address: "6220 Libero Ln",
+    city: "Trinity",
+    state: "KS",
+    zip: 61787
+  },
+  {
+    no: 2,
+
+    fname: "Penny",
+    lname: "Rende",
+    tel: "(752)880-5546",
+    address: "901 Porttitor Ln",
+    city: "Patterson",
+    state: "MO",
+    zip: 60968
+  },
+  {
+    no: 3,
+
+    fname: "Sherrie",
+    lname: "Grimsley",
+    tel: "(918)930-5946",
+    address: "9894 Sollicitudin St",
+    city: "Ashburn",
+    state: "WA",
+    zip: 93328
+  },
+  {
+    no: 4,
+
+    fname: "Arun",
+    lname: "Young",
+    tel: "(114)476-8416",
+    address: "2005 Magna Dr",
+    city: "Flushing",
+    state: "CO",
+    zip: 84399
+  },
+  {
+    no: 5,
+    fname: "Iris",
+    lname: "Pillai",
+    tel: "(212)353-3013",
+    address: "8660 Placerat Ave",
+    city: "Parker",
+    state: "PA",
+    zip: 36565
+  }
+];
