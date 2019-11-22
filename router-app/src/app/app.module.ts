6361652ad47ec3adf2ed26ee78d0834c5cc2a9bf
leelavathikm1 <leelavathikm1@gmail.com>
2019-11-22 17:14:50 +0530

    Adding Router App

---
+++ router-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  }
+];
+@NgModule({
+  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
